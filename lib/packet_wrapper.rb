5df6840088464fc4f8ddff3e9535b33d599326a8
mabasasi <aoe317293@yahoo.co.jp>
2016-12-25 00:04:51 +0900

    [fix] test

--- lib/packet_wrapper.rb
+++ lib/packet_wrapper.rb
@@ -21,6 +21,7 @@ class PacketWrapper
     #if defined? packet_in.data.target_protocol_address
     #  puts "abc -> #{nil? data.target_protocol_address}"
     #end
+    p "sub :: "
     puts (defined? packet_in.data.target_protocol_address) == nil
 
     # IPブロック
