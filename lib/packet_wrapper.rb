511361266d83b96aecc6bd40245d4b8afb5d223e
mabasasi <aoe317293@yahoo.co.jp>
2016-12-24 21:28:57 +0900

    [fix] test

--- lib/packet_wrapper.rb
+++ lib/packet_wrapper.rb
@@ -18,7 +18,7 @@ class PacketWrapper
 
 
     # ARPブロック
-    a = (defined? packet_in.target_protocol_address)
+    a = (defined? packet_in.target_protocol_address != nil)
     puts "t1 #{a}"
     puts "spa -> #{packet_in.data.target_protocol_address}" if defined? packet_in.data.target_protocol_address != nil
 
